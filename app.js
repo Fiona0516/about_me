6bb84cad6ef14c24a548aa6e01932b02486eadc7
Fiona <fchang16@gmail.com>
2016-07-19 14:27:48 -0700

    add two questions and content in html

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert('Wooooo!!');
+'use strict';
+var answer1 = prompt('Does Fiona have a cat?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert('Wrong! Fiona actually does not have a pet!');
+} else if (answer1 === 'no' || answer1 === 'n'){
+  alert('Right. She really wants to have one.');
+} else {
+  alert('That\s not an answer!');
+};
+var answer2 = prompt('Can she speak Japanese?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y'){
+  alert('Wrong. She doesn\t know how');
+} else if (answer2 === 'no' || answer2 === 'n'){
+  alert('Right. But she can read some characters because they are similar to Chinese characters!');
+} else {
+  alert('Tha\s not an answer!');
+};
